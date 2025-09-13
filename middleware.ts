b751d35cbb2f6b5fae6a2eb0652c0b29c561e4a8
Gagun45 <selyanchyn45@gmail.com>
2025-09-13 17:51:35 +0300

    middleware fixed

--- middleware.ts
+++ middleware.ts
@@ -2,6 +2,7 @@ import { NextResponse, type NextRequest } from "next/server";
 
 export function middleware(req: NextRequest) {
   const url = req.nextUrl;
+
   if (url.pathname.startsWith("/create")) {
     const accessKey = req.cookies.get("accessKey")?.value;
     if (accessKey !== process.env.NEXT_PUBLIC_ACCESS_KEY) {
